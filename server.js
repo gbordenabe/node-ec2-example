5b318a25f912626b407addca863ab85a1549f8ea
Gabriel Bordenabe <gabrielbordenabe@gmail.com>
2025-12-31 11:39:33 -0300

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req,res)=>res.send('Hello world with express!'))
+app.get('/', (req,res)=>res.send('<h1>Express con HTML!!</h1>'))
 
-app.listen(3000)
-console.log('Server running on port 3000');
+app.listen(8080)
+console.log('Server running on port 8080');
